81152136781ca38b2f43e5632b978f934593a360
Tomas Molnar <tomas.molnar@ayi.group>
2022-08-03 11:52:01 -0300

    entitys final

--- src/main/java/com/alkemy/aceleracion/disney/entity/PeliculaEntity.java
+++ src/main/java/com/alkemy/aceleracion/disney/entity/PeliculaEntity.java
@@ -4,18 +4,7 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
--- src/main/java/com/alkemy/aceleracion/disney/entity/PersonajeEntity.java
+++ src/main/java/com/alkemy/aceleracion/disney/entity/PersonajeEntity.java
@@ -3,13 +3,7 @@ package com.alkemy.aceleracion.disney.entity;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
